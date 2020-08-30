f91d04db9ca32032a26a2b730a7368e6cf388d78
matthung0807 <bronx0807@gmail.com>
2020-08-30 20:21:08 +0800

    fix

--- src/main/java/com/abc/demo/service/MessageService.java
+++ src/main/java/com/abc/demo/service/MessageService.java
@@ -1,7 +1,6 @@
 package com.abc.demo.service;
 
 import com.abc.demo.mq.config.RabbitMqConfig;
-import com.abc.demo.mq.receiver.Receiver;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +8,8 @@ import org.springframework.stereotype.Service;
 public class MessageService {
 
     private final RabbitTemplate rabbitTemplate;
-    private final Receiver receiver;
 
-    public MessageService(Receiver receiver, RabbitTemplate rabbitTemplate) {
-        this.receiver = receiver;
+    public MessageService(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
