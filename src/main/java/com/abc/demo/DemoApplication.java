adc181733b83e7483c67aa2af574e480288e599c
matthung0807 <bronx0807@gmail.com>
2020-09-20 11:53:51 +0800

    compoment scan specific packages

--- src/main/java/com/abc/demo/DemoApplication.java
+++ src/main/java/com/abc/demo/DemoApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {
-        "com.abc.demo.controller",
-        "com.abc.demo.service",
-        "com.abc.demo.repository"
-})
+//@ComponentScan(basePackages = {
+//        "com.abc.demo.controller",
+//        "com.abc.demo.service",
+//        "com.abc.demo.repository"
+//})
 @SpringBootApplication(scanBasePackages = {
         "com.abc.demo.controller",
         "com.abc.demo.service",
