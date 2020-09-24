a7a5548dbbbe107090bcc4f3ca1d21a6abea52b8
matthung0807 <bronx0807@gmail.com>
2020-09-24 23:17:39 +0800

    @ConstructorBinding

--- src/main/java/com/abc/demo/DemoApplication.java
+++ src/main/java/com/abc/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.abc.demo;
 
 import com.abc.demo.properties.SystemProperties;
+import com.abc.demo.service.DemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 
+@EnableConfigurationProperties(SystemProperties.class)
 @PropertySource("classpath:system.properties")
 @SpringBootApplication
 public class DemoApplication {
@@ -13,12 +16,9 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-        SystemProperties systemProperties = ctx.getBean(SystemProperties.class);
+        DemoService demoService = ctx.getBean(DemoService.class);
 
-        System.out.println(systemProperties.getName());
-        System.out.println(systemProperties.getVersion());
-        System.out.println(systemProperties.getUrl());
-        System.out.println(systemProperties.getPort());
+        demoService.printSystemPropertiesValue();
 
     }
 
