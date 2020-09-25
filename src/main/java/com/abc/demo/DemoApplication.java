c323379a2318efc70fdb5cdafa92ee6b8062a6f1
matthung0807 <bronx0807@gmail.com>
2020-09-25 10:35:51 +0800

    @ConstructorBinding

--- src/main/java/com/abc/demo/DemoApplication.java
+++ src/main/java/com/abc/demo/DemoApplication.java
@@ -4,11 +4,13 @@ import com.abc.demo.properties.SystemProperties;
 import com.abc.demo.service.DemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 
 @EnableConfigurationProperties(SystemProperties.class)
+//@ConfigurationPropertiesScan
 @PropertySource("classpath:system.properties")
 @SpringBootApplication
 public class DemoApplication {
