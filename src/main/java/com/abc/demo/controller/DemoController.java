879ce9a6fa2686a9549815e36e2d1061ad14cde4
matthung0807 <bronx0807@gmail.com>
2020-12-12 18:13:18 +0800

    Spring Boot Jib Maven build docker image

--- src/main/java/com/abc/demo/controller/DemoController.java
+++ src/main/java/com/abc/demo/controller/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World Spring Boot build by Jenkins Maven plugin";
+        return "Hello World Spring Boot build by Jib Maven plugin";
     }
 }
