632f4c7e7387ea2295615368fddb86f92ffc329d
matthung0807 <bronx0807@gmail.com>
2021-05-24 09:02:44 +0800

    expressions on selections

--- src/main/java/com/abc/demo/controller/DemoController.java
+++ src/main/java/com/abc/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.abc.demo.controller;
 
+import com.abc.demo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,14 @@ public class DemoController {
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
             Model model) {
         model.addAttribute("name", name); // set request attribute
+
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("John");
+        employee.setAge(29);
+
+        model.addAttribute("employee", employee);
+
         return "hello"; // forward to hello.html
     }
 }
