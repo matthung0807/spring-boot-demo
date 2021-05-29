12c770a6fc77243e9aab77b1926894bbfac32e57
matthung0807 <bronx0807@gmail.com>
2021-05-29 11:49:14 +0800

    fixE

--- src/main/java/com/abc/demo/controller/TodoController.java
+++ src/main/java/com/abc/demo/controller/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TodoController {
@@ -47,8 +48,8 @@ public class TodoController {
         return "redirect:/todolist";
     }
 
-    @GetMapping(value= "/todolist/delete/{id}")
-    public String delete(@PathVariable long id) {
+    @PostMapping(value= "/todolist/delete")
+    public String delete(@RequestParam long id) {
         todoService.delete(id);
         return "redirect:/todolist";
     }
@@ -64,7 +65,11 @@ public class TodoController {
     @PostMapping(value = "/todolist/update")
     public String update(@RequestParam long id, @RequestParam String desc, Model model) {
         if (StringUtils.isEmpty(desc)) {
-            return "redirect:/todolist/update/" + id;
+            TodoModel todoModel = todoService.getById(id);
+            model.addAttribute("id", todoModel.getId());
+            model.addAttribute("desc", todoModel.getDesc());
+            model.addAttribute("error", "內容不可為空");
+            return "update";
         }
         todoService.update(id, desc);
         return "redirect:/todolist";
