a5fd52ae37448dee0d3e6399e5d005bded31a8bb
matt hung <bronx0807@gmail.com>
2026-01-13 11:02:55 +0800

    google map routes api simple

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # spring-boot-demo
 Spring Boot Demo
 
-本專案為簡單的Spring Boot 3專案。
-
-# Dependencies
-- [Spring Web](https://docs.spring.io/spring-boot/reference/web/index.html)
-- [Spring Boot Devtools](https://docs.spring.io/spring-boot/reference/using/devtools.html)
-- [Lombok](https://projectlombok.org/)
+[Java Google Map Platform Routes API簡單範例](https://matthung0807.blogspot.com/2026/01/java-google-map-platform-routes-api-simple.html)
--- src/main/java/com/abc/demo/DemoApplication.java
+++ src/main/java/com/abc/demo/DemoApplication.java
@@ -1,13 +1,46 @@
 package com.abc.demo;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoApplication.class, args);
+
+        String apiKey = "API_KEY";
+
+        String requestBody = new ObjectMapper().writeValueAsString(Map.of(
+                "origin", Map.of("address", "桃園市桃園區大同路100號"),
+                "destination", Map.of("address", "台北市內湖區瑞光路515號"),
+                "travelMode", "TWO_WHEELER"));
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://routes.googleapis.com/directions/v2:computeRoutes"))
+                .header("Content-Type", "application/json")
+                .header("X-Goog-Api-Key", apiKey)
+                .header("X-Goog-FieldMask", String.join(",",
+                        "routes.distanceMeters",
+                        "routes.duration"))
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response;
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String responseBody = response.body();
+        System.out.println("Response Body: " + responseBody);
+
+    }
 
 }
