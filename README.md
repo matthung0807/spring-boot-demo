22250529438461f31ed96cb77628d58fe70b6748
matt hung <bronx0807@gmail.com>
2026-01-15 10:40:07 +0800

    google maps static api

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # spring-boot-demo
 Spring Boot Demo
 
-[Java Google Map Platform Routes API簡單範例](https://matthung0807.blogspot.com/2026/01/java-google-map-platform-routes-api-simple.html)
+[Java Google Map Platform Maps Static API 範例](https://matthung0807.blogspot.com/2026/01/java-google-map-platform-maps-static-api.html)
--- src/main/java/com/abc/demo/DemoApplication.java
+++ src/main/java/com/abc/demo/DemoApplication.java
@@ -1,14 +1,21 @@
 package com.abc.demo;
 
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
@@ -19,28 +26,84 @@ public class DemoApplication {
 
         String apiKey = "API_KEY";
 
-        String requestBody = new ObjectMapper().writeValueAsString(Map.of(
-                "origin", Map.of("address", "桃園市桃園區大同路100號"),
-                "destination", Map.of("address", "台北市內湖區瑞光路515號"),
-                "travelMode", "TWO_WHEELER"));
+        String origin = "桃園市桃園區大同路100號";
+        String destination = "台北市內湖區瑞光路515號";
+
+        String encodedPolyline = getRouteEncodedPolyline(origin, destination, apiKey);
+
+        String url = buildMapUri(origin, destination, encodedPolyline, apiKey);
+        System.out.println("url: " + url);
+
+        saveStaticMapImage(url);
+
+    }
+
+    private static String getRouteEncodedPolyline(String origin, String destination, String apiKey) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String requestBody = mapper.writeValueAsString(Map.of(
+                "origin", Map.of("address", origin),
+                "destination", Map.of("address", destination),
+                "travelMode", "DRIVE"));
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://routes.googleapis.com/directions/v2:computeRoutes"))
                 .header("Content-Type", "application/json")
                 .header("X-Goog-Api-Key", apiKey)
-                .header("X-Goog-FieldMask", String.join(",",
-                        "routes.distanceMeters",
-                        "routes.duration"))
+                .header("X-Goog-FieldMask", String.join(",", "routes.polyline"))
                 .POST(HttpRequest.BodyPublishers.ofString(requestBody))
                 .build();
 
         HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response;
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String responseBody = response.body();
-        System.out.println("Response Body: " + responseBody);
+        JsonNode root = mapper.readTree(response.body());
+        String encodedPolyline = root.path("routes")
+                .path(0)
+                .path("polyline")
+                .path("encodedPolyline")
+                .asText("");
+        return encodedPolyline;
+    }
 
+    private static String buildMapUri(String origin, String destination, String encodedPolyline, String apiKey)
+            throws Exception {
+        String markerA = URLEncoder.encode(
+                "color:red|label:A|" + origin,
+                StandardCharsets.UTF_8);
+
+        String markerB = URLEncoder.encode(
+                "color:blue|label:B|" + destination,
+                StandardCharsets.UTF_8);
+
+        String path = URLEncoder.encode(
+                "enc:" + encodedPolyline,
+                StandardCharsets.UTF_8);
+
+        String url = "https://maps.googleapis.com/maps/api/staticmap?"
+                + "size=640x640"
+                + "&markers=" + markerA
+                + "&markers=" + markerB
+                + "&path=" + path
+                + "&key=" + apiKey;
+
+        return url;
+    }
+
+    private static void saveStaticMapImage(String staticMapUrl) throws Exception {
+        HttpRequest imgReq = HttpRequest.newBuilder()
+                .uri(URI.create(staticMapUrl))
+                .GET()
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<InputStream> imgRes = client.send(imgReq, HttpResponse.BodyHandlers.ofInputStream());
+
+        try (InputStream is = imgRes.body()) {
+            Files.copy(
+                    is,
+                    Path.of("map.png"),
+                    StandardCopyOption.REPLACE_EXISTING);
+        }
     }
 
 }
